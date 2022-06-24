d5ad0214f090292a3031d23daddf2a321d931b91
yimin pan <yimin.pan.123@gmail.com>
2022-06-24 21:54:14 +0200

    BFM2017

---
+++ scripts/extractBFM.py
@@ -0,0 +1,19 @@
+"""
+For this script to work, model2017-1_face12_nomouth.h5 file should be placed in the /faceModels/BFM2017/ directory. 
+This dataset can be downloaded from https://faces.dmi.unibas.ch/bfm/
+"""
+
+import numpy as np
+import h5py
+
+pathToDataset = "../faceModels/BFM2017/"
+path = pathToDataset + "model2017-1_face12_nomouth.h5"
+
+with h5py.File(path, 'r') as f:
+    for k, i in f.items():
+        if k in ['shape', 'color','expression']:
+            for k2, i2 in i['model'].items():
+                if k2 in ['mean', 'pcaVariance', 'pcaBasis']:
+                    np_array = np.array(i2)
+                    outFilePath = pathToDataset + "BFM2017_" + k + "_" + k2 + ".txt"
+                    np.savetxt(outFilePath, np_array)
