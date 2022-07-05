80dec4c78681eb7a9da28303dbcdb0149570c1c3
weixiao <wei.xiao.xia@estudiantat.upc.edu>
2022-07-05 20:24:31 +0200

    Rendered optimized with opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 #include "Optimizer.h"
 #include "Face.h"
-#include "Render.h"
+#include "Renderer.h"
 #include <chrono>
 
 using namespace std;
+
+// singleton class
+Renderer Renderer::s_instance;
 
 vector<string> taskOptions{ "Face reconstruction", "Expression transfer", "Rasterize random face" };
 int taskOption = -1;
@@ -56,15 +59,17 @@ void performTask() {
 	}
 	case 3:
 	{
+		auto& renderer = Renderer::Get();
 		float scale_factor = 1 / 100.f;
-		FaceModel face_model = FaceModel("BFM17");
-		MatrixXf coords = face_model.getShapeMean().reshaped(3, face_model.getNumVertices()).transpose();
+		Face face;
+		face.randomizeParameters();
+		MatrixXf coords = face.calculateVerticesDefault().reshaped(3, face.getFaceModel().getNumVertices()).transpose();
 		Vector3f mean = coords.colwise().mean();
 		Matrix4f projection_matrix = Matrix4f::Identity() * scale_factor;
 		projection_matrix.block(0, 3, 3, 1) = -mean * scale_factor;
 		projection_matrix(3, 3) = 1.f;
 		const auto start = std::chrono::steady_clock::now();
-		cv::Mat img = render(face_model, projection_matrix, 720, 720);
+		cv::Mat img = renderer.render(face, projection_matrix, 360, 360);
 		const auto end = std::chrono::steady_clock::now();
 		std::cout << "time used: " << std::to_string(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()) << "ms\n";
 		cv::imwrite("../../data/samples/2d face image/sample_image.png", img);
