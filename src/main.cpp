32bc11a17e346ace3306131490b4e04bc6ea5de1
weixiao <wei.xiao.xia@estudiantat.upc.edu>
2022-07-13 14:54:34 +0200

    Minor

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@ int taskOption = -1;
 // vector<string> inputOptions{ "Use sample image(s)" };
 // int inputOption = -1;
 
+Renderer Renderer::s_instance;
+
 void handleMenu() {
 	cout << "Please select a task:\n";
 	for (int i = 0; i < taskOptions.size(); i++) cout << i + 1 << ". " << taskOptions[i] << endl;
@@ -60,26 +62,17 @@ void performTask() {
 	case 3:
 	{
 		auto& renderer = Renderer::Get();
-		Face face = Face("sample1", "BFM17");
-		// randomize for testing
-		optimizer.optimize(face);
-		Matrix4f projection_matrix = face.getExtrinsics().cast<float>();
-		Matrix4f intrinsics = Matrix4f::Identity();
-		intrinsics.block(0, 0, 3, 3) = face.getIntrinsics().cast<float>();
-		projection_matrix = intrinsics * projection_matrix;
-		/*Face face;
-		face.randomizeParameters();*/
+		Face face;
+		face.randomizeParameters();
 		renderer.initialiaze_rendering_context(face.getFaceModel(), 720, 720);
 
 		// Initialize the default projection matrix which moves the face to origin
 		double scale_factor = 1 / 100.;
 		MatrixXd coords = face.calculateVerticesDefault().reshaped(3, face.getFaceModel().getNumVertices()).transpose();
 		Vector3d mean = coords.colwise().mean();
-		/*Matrix4d projection_matrix_ = Matrix4d::Identity() * scale_factor;
-		projection_matrix_.block(0, 3, 3, 1) = -mean * scale_factor;
-		projection_matrix_(3, 3) = 1.;*/
-		cout << mean << endl;
-		cout << face.getExtrinsics().block(0, 3, 3, 1) << endl;
+		Matrix4d projection_matrix = Matrix4d::Identity() * scale_factor;
+		projection_matrix.block(0, 3, 3, 1) = -mean * scale_factor;
+		projection_matrix(3, 3) = 1.;
 		
 		VectorXf vertices = face.calculateVerticesDefault().cast<float>();
 		VectorXf colors = face.calculateColorsDefault().cast<float>();
@@ -87,9 +80,9 @@ void performTask() {
 		renderer.render(face, projection_matrix.transpose().cast<float>(), vertices, colors);
 
 		cv::Mat depth_to_visualize;
-		cv::normalize(std::get<1>(buffers), depth_to_visualize, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+		cv::normalize(renderer.get_depth_buffer(), depth_to_visualize, 0, 255, cv::NORM_MINMAX, CV_8UC1);
 		cv::imwrite("../../data/samples/2d face image/sample_image_depth.png", depth_to_visualize);
-		cv::imwrite("../../data/samples/2d face image/sample_image.png", std::get<0>(buffers));
+		cv::imwrite("../../data/samples/2d face image/sample_image.png", renderer.get_color_buffer());
 		break;
 	}
 	default:
